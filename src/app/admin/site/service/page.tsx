af9c3f97ad38b7951b85a93942ce1cfffab7d256
Will-Key <saibe.akreiry@gmail.com>
2025-04-29 20:39:51 +0000

    Add rich text editor

--- src/app/admin/site/service/page.tsx
+++ src/app/admin/site/service/page.tsx
@@ -50,7 +50,7 @@ export default function Page() {
               Créer un service
             </Button>
           </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
+          <DialogContent className="sm:max-w-[650px]">
             <DialogHeader>
               <DialogTitle>
                 {serviceToEdit ? "Modifier" : "Créer"} un service
